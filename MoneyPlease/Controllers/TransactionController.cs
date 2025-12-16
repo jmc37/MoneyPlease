b213864bbc38fcc5fc93be1a9b52c0dcba9199da
Joseph Chan <74024192+jmc37@users.noreply.github.com>
2025-12-15 20:56:11 -0800

    Created transaction controller and service

---
+++ MoneyPlease/Controllers/TransactionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyPlease.Controllers
+{
+    [ApiController]
+    [Route("api/transaction")]
+    public class TransactionController : Controller
+    {
+
+        public IActionResult GetTransaction(string transactionId)
+        {
+            return View();
+        }
+
+        public IActionResult CreateTransaction()
+        {
+            return View();
+        }
+        public IActionResult UpdateTransaction(string transactionId)
+        {
+            return View();
+        }
+        public IActionResult DeleteTransaction(string transactionId)
+        {
+            return View();
+        }
+    }
+}
