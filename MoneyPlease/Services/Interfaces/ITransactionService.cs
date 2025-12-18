e486d1d48d784f62151529d6df25fb19623ce1e0
Joseph Chan <74024192+jmc37@users.noreply.github.com>
2025-12-17 22:01:26 -0800

    updated transaction interface

--- MoneyPlease/Services/Interfaces/ITransactionService.cs
+++ MoneyPlease/Services/Interfaces/ITransactionService.cs
@@ -1,7 +1,14 @@
-﻿namespace MoneyPlease.Services.Interfaces
+﻿using MoneyPlease.Dtos.Transaction;
+
+namespace MoneyPlease.Services.Interfaces
 {
     public interface ITransactionService
     {
+        public Task GetTransaction(string transactionId);
+        public Task CreateTrasaction(CreateTransactionDto dto);
+
+        public Task UpdateTransaction(CreateTransactionDto transaction);
+        public Task DeleteTrasaction(string transactionId);
 
     }
 }
